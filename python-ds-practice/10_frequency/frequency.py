8de184a07828c57ab778260b8925e814fd0d64a8
Marlon Mariani <marlon.j.mariani@gmail.com>
2020-06-13 15:12:13 -0400

    Add files via upload

---
+++ python-ds-practice/10_frequency/frequency.py
@@ -0,0 +1,4 @@
+def frequency(list, search_term):
+    
+
+    return list.count(search_term)
---
+++ python-ds-practice/14_compact/compact.py
@@ -0,0 +1,8 @@
+def compact(list):
+    """Return a copy of lst with non-true elements removed.
+
+        >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
+        [1, 2, 'All done']
+    """
+
+    return [val for val in list if val]
